fd7dcea6f432007859b69cb203dfb9959c0a08f8
zuqiang.you <you1115879184@126.com>
2023-03-05 00:15:51 +0800

    更新d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,30 +1,34 @@
-
-import * as React from 'react';
+import * as React from "react";
+import { AxiosResponse } from "axios";
 
 interface ApisProps {
-  [key: string]: any
+  [key: string]: any;
 }
 
 interface ConnectApiProps {
-  scope: string | string[],
-  isScope?: boolean,
-  resultMode?: string
+  scope: string | string[];
+  isScope?: boolean;
+  resultMode?: string;
 }
 
 interface ProviderProps {
-  apis: string[],
-  context?: any,
-  children?: React.ReactNode
+  apis: Record<string, unknown>;
+  context?: any;
+  children?: React.ReactNode;
 }
 
-interface ConfigProps {
-  resultMode?: string,
-  prefix?: string,
+export interface ConfigProps {
+  resultMode?: string;
+  prefix?: string;
+  adapter?: (config: any) => Promise<void | AxiosResponse<any, any>>;
 }
 
 export declare function combineApi(apis: ApisProps, isScope?: boolean): any;
 
-export declare function connectApi(WrapperComponent: React.Component | string | string[], scope?: string | string[] | ConnectApiProps): any;
+export declare function connectApi(
+  WrapperComponent: React.Component | string | string[],
+  scope?: string | string[] | ConnectApiProps
+): any;
 
 export declare function ProviderApi(obj: ProviderProps): any;
 
